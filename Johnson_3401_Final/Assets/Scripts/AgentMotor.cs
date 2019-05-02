0aa6e0b6f62ee6b680c8d70931e30f16395a1825
aj418014 <47157670+aj418014@users.noreply.github.com>
2019-05-02 13:29:32 -0400

    Added Unity Project

---
+++ Johnson_3401_Final/Assets/Scripts/AgentMotor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMotor : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Vector3 _target;
+    NavMeshAgent _agent;
+    public Transform targetObject;
+    public Transform origin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _agent.speed = speed;
+        _agent.SetDestination(_target);
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(cameraRay, out hit))
+            {
+                origin.position = transform.position;
+                Debug.Log(hit.point);
+                _target = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                targetObject.transform.position = _target;
+            }
+        }
+
+    }
+}
